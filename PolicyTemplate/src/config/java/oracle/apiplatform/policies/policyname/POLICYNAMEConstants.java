20e3ff96feeaeab1f91639912b69964bd1f8149b
phil@mp3monster.org <jG8EoPfaTpU>
2018-04-03 17:42:59 +0100

    relocated the template tool into its on repository

--- PolicyTemplate/src/config/java/oracle/apiplatform/policies/policyname/POLICYNAMEConstants.java
+++
@@ -1,9 +0,0 @@
-package oracle.apiplatform.policies.%%policyname%%;
-
-public interface %%POLICYNAME%%Constants {
-	
-	public static final String POLICY_NAME = "POLICYNAME";
-	
-	public static final String EXECUTION_FAILURE = "EXECUTION_FAILURE";
-	
-}
